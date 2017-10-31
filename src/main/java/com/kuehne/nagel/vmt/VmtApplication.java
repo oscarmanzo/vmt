ce38775a5308e4c10c4a1b5a66886f69ffffd686
oscarmanzo <oscar.manzo.c@gmail.com>
2017-10-31 00:22:15 -0600

    first commit

---
+++ src/main/java/com/kuehne/nagel/vmt/VmtApplication.java
@@ -0,0 +1,12 @@
+package com.kuehne.nagel.vmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmtApplication.class, args);
+	}
+}
